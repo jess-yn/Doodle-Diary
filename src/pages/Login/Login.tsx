4c35c357187a602837758f93ea3257469e920a96
jess-yn <jess986.codes@gmail.com>
2026-05-25 21:49:40 -0700

    fix mobile layout for two column sections

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,18 @@
 import { LoginForm } from "../../components/Form/LoginForm";
 import { FileStack } from "../../components/FileStack/FileStack";
+import "./Login.css";
 
 export function Login() {
   return (
     <>
-      <div className="two-column">
-        <div className="left">
-          <FileStack></FileStack>
-        </div>
-        <div className="right">
-          <LoginForm></LoginForm>
+      <div className="form-page">
+        <div className="two-column">
+          <div className="left">
+            <FileStack></FileStack>
+          </div>
+          <div className="right">
+            <LoginForm></LoginForm>
+          </div>
         </div>
       </div>
     </>
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,15 +1,18 @@
 import { FileStack } from "../../components/FileStack/FileStack";
 import { SignUpForm } from "../../components/Form/SignUpForm";
+import "../Login/Login.css";
 
 export function SignUp() {
   return (
     <>
-      <div className="two-column">
-        <div className="left">
-          <FileStack></FileStack>
-        </div>
-        <div className="right">
-          <SignUpForm></SignUpForm>
+      <div className="form-page">
+        <div className="two-column">
+          <div className="left">
+            <FileStack></FileStack>
+          </div>
+          <div className="right">
+            <SignUpForm></SignUpForm>
+          </div>
         </div>
       </div>
     </>
